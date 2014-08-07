1018a6c189d906fbb6e68cbc77a8c27571dfae9b
Robert Beach <rdbeach@gmail.com>
2014-08-07 01:32:03 -0700

    Create JsonQueryUtil.java

---
+++ JsonQueryUtil.java
@@ -0,0 +1,12 @@
+
+public class JsonQueryUtil {
+	public static boolean isInteger( String input ) {
+	    try {
+	        Integer.parseInt( input );
+	        return true;
+	    }
+	    catch( Exception e ) {
+	        return false;
+	    }
+	}
+}
