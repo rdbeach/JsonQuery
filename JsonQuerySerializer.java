7176865f8b41b5820c67558bbdd31e34f8989e26
Robert Beach <rdbeach@gmail.com>
2014-08-06 19:51:35 -0700

    Create JsonQuerySerializer.java

---
+++ JsonQuerySerializer.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Type;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+
+@SuppressWarnings("rawtypes")
+public class JsonQuerySerializer implements JsonSerializer<JsonQuery> {
+
+  public JsonElement serialize(final JsonQuery json, final Type typeOfT, final JsonSerializationContext context)
+      throws JsonParseException {
+	
+	  return context.serialize(json.node);
+  }
+
+}
